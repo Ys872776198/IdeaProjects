225e4bf03cf913afbcf58db808a502cb65716b0c
ys <872776198@qq.com>
2021-07-22 23:28:03 +0800

    2021/7/22 23:27

--- ExciseCodes/src/BiLIExciseTest/E61_Test.java
+++ ExciseCodes/src/BiLIExciseTest/E61_Test.java
@@ -1,10 +1,56 @@
 package BiLIExciseTest;
 
+import BiLiExcisesCodes.E61;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * @Auther: YS
  * @Date: 2021/7/22
- * @Description: IntelliJ IDEA
+ * @Description: Junit（接E61）
  * @Version: 1.0
  */
 public class E61_Test {
+    /**
+     * 在测试方法下加一个@Test的注解
+     * 需要导入Junit的包
+     * 测试方法建议无参，无返回值
+     * 可以加入断言：让时间结果和预期结果进行对比，判断代码是否运行正确
+     *
+     * 注解@Before：会在测试方法运行前执行
+     * 一般会在@Before里面加一些申请资源的代码：申请数据库资源，申请IO资源，申请网络资源
+     *
+     * 注解@After：会在测试方法结束后执行
+     * 一般会在@After里面加一些释放资源的代码：释放数据库资源，释放IO资源，释放网络资源
+     *
+     * 框架 = 注解 + 反射 + 设计结构
+     *
+     */
+
+    @Before
+    public void Strat(){
+        System.out.println("测试方法开始了");
+    }
+
+    @After
+    public void End(){
+        System.out.println("测试方法结束了");
+    }
+
+    @Test
+    public void AddTest(){
+        E61 e = new E61();
+        Assert.assertEquals(60, e.Add(50, 10));         //入参: 预测结果，实际结果
+    }
+
+    @Test
+    public void SubTest(){
+        E61 e = new E61();
+        e.Sub(50, 10);
+        Assert.assertEquals(60, e.Sub(50, 10));         //入参: 预测结果，实际结果
+    }
+
+
 }
--- ExciseCodes/src/BiLiExcisesCodes/E61.java
+++ ExciseCodes/src/BiLiExcisesCodes/E61.java
@@ -3,8 +3,28 @@ package BiLiExcisesCodes;
 /**
  * @Auther: YS
  * @Date: 2021/7/22
- * @Description: Junit
+ * @Description: Junit（接E61_Test）
  * @Version: 1.0
  */
+
+//Junit属于白盒测试
+/**
+ * 在没有使用Junit的时候，缺点：
+ * （1）测试一定走main方法，是程序的入口，main方法的格式必须不能写错。
+ * （2）要是在同一个main方法中测试的话，那么不需要测试的东西必须注释掉。
+ * （3）测试逻辑如果分开。需要定义多个测试类，比较麻烦。
+ * （4）业务逻辑和测试代码，都混淆了。
+ */
+
 public class E61 {
+    public int Add(int a, int b){
+        System.out.println("相加的结果：" + (a + b));
+        return (a + b);
+    }
+
+    public int Sub(int a, int b){
+//        System.out.println("相减的结果：" + (a - b));
+        return (a - b);
+    }
+
 }
